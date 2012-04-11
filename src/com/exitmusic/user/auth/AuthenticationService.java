d25434cfeb6e5c9ee11e02b0c2a3450e1b4318d6
Kai Chang <kai.h.chang@gmail.com>
2012-04-11 15:13:25 -0500

    Moved classes into packages

---
+++ src/com/exitmusic/user/auth/AuthenticationService.java
@@ -0,0 +1,31 @@
+package com.exitmusic.user.auth;
+/**
+ * Description: Description goes here.
+ * 
+ * @author <a href="mailto:123fakestreet">Kai Chang</a>
+ * @version $Revision$ $Date$
+ * @since 0.1
+ */
+public interface AuthenticationService {
+
+	/**
+	 * Create user account
+	 * 
+	 * @param userId
+	 */
+	void create(String userId);
+
+	/**
+	 * Delete user account
+	 * 
+	 * @param userId
+	 */
+	void delete(String userId);
+
+	/**
+	 * Login user
+	 * 
+	 * @param userId
+	 */
+	void login(String userId);
+}
---
+++ src/com/exitmusic/user/auth/AuthenticationServiceImpl.java
@@ -0,0 +1,47 @@
+package com.exitmusic.user.auth;
+import com.exitmusic.user.account.UserAccountDAO;
+
+/**
+ * Description: Description goes here.
+ * 
+ * @author <a href="mailto:123fakestreet">Kai Chang</a>
+ * @version $Revision$ $Date$
+ * @since 0.1
+ */
+public class AuthenticationServiceImpl
+	implements AuthenticationService
+{
+
+	private UserAccountDAO _userAccountDAO;
+
+	/**
+	 * {@inheritDoc}
+	 * 
+	 * @see AuthenticationService#create(java.lang.String)
+	 */
+	public void create(String userId) {
+		if (!_userAccountDAO.lookupById(userId)) {
+			_userAccountDAO.saveUser(userId);
+		}
+	}
+
+	/**
+	 * {@inheritDoc}
+	 * 
+	 * @see AuthenticationService#delete(java.lang.String)
+	 */
+	public void delete(String userId) {
+		if (_userAccountDAO.lookupById(userId)) {
+			_userAccountDAO.deleteUser(userId);
+		}
+	}
+
+	/**
+	 * {@inheritDoc}
+	 * 
+	 * @see AuthenticationService#login(java.lang.String)
+	 */
+	public void login(String userId) {
+		// login code
+	}
+}
