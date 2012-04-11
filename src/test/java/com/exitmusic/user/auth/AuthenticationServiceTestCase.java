d403b54055b3eeecd1f18fe78530b427b545f7be
Kai Chang <kai.h.chang@gmail.com>
2012-04-11 16:01:11 -0500

    Added AuthenticationService test class

---
+++ src/test/java/com/exitmusic/user/auth/AuthenticationServiceTestCase.java
@@ -0,0 +1,13 @@
+
+package com.exitmusic.user.auth;
+
+/**
+ * Description: Description goes here.
+ * 
+ * @author <a href="mailto:123fakestreet">Kai Chang</a>
+ * @version $Revision$ $Date$
+ * @since 0.1
+ */
+public class AuthenticationServiceTestCase {
+
+}
